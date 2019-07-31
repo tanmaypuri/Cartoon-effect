77a2c5d5d36f8de6270f443760d334893edc6775
tanmaypuri <52965504+tanmaypuri@users.noreply.github.com>
2019-07-31 15:26:02 +0530

    Update cartoon.py

--- cartoon.py
+++ cartoon.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("adventure.jpeg")
+img = cv2.imread("Path of the image")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
